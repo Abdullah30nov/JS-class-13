40d7f24f42534d2312ce798927173d9dd99c44c5
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-04-26 21:21:13 +0500

    Object Method

---
+++ app.js
@@ -0,0 +1,19 @@
+// var text = "how are you doing?"
+
+// var split = text.split(" ");
+
+// var combine = text.join(",")
+
+// console.log(combine)
+
+
+
+var studentobj = {
+    name: "Abdullah",
+    age: "17",
+    email: "abdullah@gmail.com",
+    address: "karachi"
+}
+
+console.log(studentobj)
+console.log(studentobj.name)
